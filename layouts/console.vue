03d70f27f4fc04e8c07604bf9a918aca11f949d6
letieu <letieu8@gmail.com>
2024-10-13 01:27:34 +0700

    init dasboard layout

---
+++ layouts/console.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="hidden md:block">
+    <Navbar />
+
+    <div class="border-t">
+      <div class="bg-background">
+        <div class="grid lg:grid-cols-5">
+          <Sidebar class="hidden lg:block" />
+
+          <div class="col-span-3 lg:col-span-4 lg:border-l">
+            <div class="h-full px-4 py-6 lg:px-8">
+              <slot />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "console",
+});
+</script>
+
+<template>
+  Okok
+</template>
