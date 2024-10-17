dfad2fbcea9fdafe4987e02859557693f36ca14f
letieu <letieu8@gmail.com>
2024-10-17 17:23:55 +0700

    fix: can't create crawler

--- server/api/crawler/index.post.ts
+++ server/api/crawler/index.post.ts
@@ -4,10 +4,17 @@ export default defineEventHandler(async (event) => {
 
   const newCrawler = await db
     .prepare(
-      'INSERT INTO Crawler (name, description, userId) VALUES ("?1", "?2", ?3)',
+      "INSERT INTO Crawler (name, description, userId) VALUES (?1, ?2, ?3)",
     )
     .bind(body.name, body.description, 1)
     .run();
+
+  if (newCrawler.error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Cannot create Crawler",
+    });
+  }
 
   return newCrawler;
 });
